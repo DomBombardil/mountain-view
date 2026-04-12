55c9d8432439f16460961a342ffc44c9f05d0085
dominik <dominik@protonmail.com>
2026-04-12 11:15:57 +0200

    Updated urls to match the new views, and added a .js file and static folder structure for later

--- nearbymountains/urls.py
+++ nearbymountains/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import mountain_search
+from .views import mountain_search, mountain_map, nearby_mountains_api
 
 urlpatterns = [
-    path("", mountain_search, name="mountain_search"),
+    path("", mountain_map, name="mountain_map"),
+    path("api/nearby_mountains/", nearby_mountains_api, name="nearby_mountains_api"),
+    path("mountain_search", mountain_search, name="mountain_search"),
 ]
